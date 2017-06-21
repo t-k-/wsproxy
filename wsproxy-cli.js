9b35b6a26dd2a563cef57870919d493e133da4e2
t-k- <clock126@126.com>
2017-06-21 18:28:40 +0800

    simplify

--- wsproxy-cli.js
+++ wsproxy-cli.js
@@ -1,17 +1,22 @@
 var net = require('net');
+var http = require('http');
 var common = require('./wsproxy-common.js');
 
 var long_socket = new net.Socket();
+var socket = new net.Socket();
+
+socket.connect(8989, '127.0.0.1', function() {
+	console.log('short socket connected.');
+});
 
 long_socket.connect(8986, '127.0.0.1', function() {
-	console.log('Connected');
 	common.setupSocket(long_socket);
+	common.setupSocket(socket);
+	console.log('Connected');
 
-	long_socket.write('Hello, server! Love, Client.');
-	long_socket.destroy();
+	socket.pipe(long_socket)
+	long_socket.pipe(socket);
 
-}).on('data', function(data) {
-	console.log('Received: ' + data);
 }).on('close', function() {
-	console.log('Connection closed');
+	console.log('Connection closed !!!!!');
 });
--- wsproxy-srv.js
+++ wsproxy-srv.js
@@ -10,8 +10,6 @@ long_server.on('connection', function(socket) {
 	common.setupSocket(socket);
 	long_socket = socket;
 
-	//socket.write('Echo server\r\n');
-
 	socket.on('error', function(err){
 		console.log('ERR: ' + err.message);
 	});
@@ -20,69 +18,81 @@ long_server.on('connection', function(socket) {
 var short_server = http.createServer().listen(8999);
 
 short_server.on('upgrade', function(req, socket, head) {
-	common.printIO('> ',
-		req['method'] + ' ' + req['url'] + '\n' +
-		common.headerLines(req),
-		socket.remoteAddress, socket.remotePort
-	);
+	common.setupSocket(socket);
+//	common.printIO('> ',
+//		req['method'] + ' ' + req['url'] + '\n' +
+//		common.headerLines(req),
+//		socket.remoteAddress, socket.remotePort
+//	);
 
     if (head && head.length) socket.unshift(head);
+	
+	var tmpMsg =
+		req.method + ' ' + req.url + '\r\n' +
+		common.headerLines(req) + '\r\n';
 
-	var proxyReqOptions = {
-		host: 'localhost:8989',
-		hostname: 'localhost',
-		port: 8989,
-		path: req.url,
-		method: req.method,
-		headers: common.extend({}, req.headers)
-	};
 
-	var proxyReq = http.request(proxyReqOptions);
-	proxyReq.end();
+	if (long_socket) {
+		long_socket.write(tmpMsg);
+		socket.pipe(long_socket)
+		long_socket.pipe(socket);
+	}
 
-	common.printIO('>>',
-		proxyReqOptions.method + ' ' + proxyReqOptions.path + '\n' +
-		common.headerLines(proxyReqOptions),
-		proxyReqOptions.hostname, proxyReqOptions.port
-	);
-
-	proxyReq.on('error', function (err) {
-		console.log('ERR: ' + err.message);
-	});
-
-	proxyReq.on('upgrade', function(proxyRes, proxySocket, proxyHead) {
-		common.setupSocket(proxySocket);
-
-		var upgradeResponse =
-			'HTTP/' + proxyRes.httpVersion + ' ' +
-			proxyRes.statusCode + ' ' +
-			proxyRes.statusMessage + '\r\n' +
-			common.headerLines(proxyRes) + '\r\n';
-
-		common.printIO('< ', upgradeResponse,
-			proxySocket.remoteAddress, proxySocket.remotePort
-		);
-
-		proxySocket.on('end', function () {
-			short_server.close();
-		});
-		proxySocket.on('error', function (err) {
-			console.log('ERR: ' + err.message);
-		});
-		socket.on('error', function () {
-			proxySocket.end();
-		});
-
-		if (proxyHead && proxyHead.length)
-			proxySocket.unshift(proxyHead);
-
-		common.printIO('<<', upgradeResponse,
-			socket.remoteAddress, socket.remotePort
-		);
-		console.log(upgradeResponse);
-
-		socket.write(upgradeResponse);
-		proxySocket.pipe(socket).pipe(proxySocket);
-		//long_socket.pipe(socket);
-	});
+//	var proxyReqOptions = {
+//		host: 'localhost:8989',
+//		hostname: 'localhost',
+//		port: 8989,
+//		path: req.url,
+//		method: req.method,
+//		headers: common.extend({}, req.headers)
+//	};
+//
+//	var proxyReq = http.request(proxyReqOptions);
+//	proxyReq.end();
+//
+//	common.printIO('>>',
+//		proxyReqOptions.method + ' ' + proxyReqOptions.path + '\n' +
+//		common.headerLines(proxyReqOptions),
+//		proxyReqOptions.hostname, proxyReqOptions.port
+//	);
+//
+//	proxyReq.on('error', function (err) {
+//		console.log('ERR: ' + err.message);
+//	});
+//
+//	proxyReq.on('upgrade', function(proxyRes, proxySocket, proxyHead) {
+//		common.setupSocket(proxySocket);
+//
+//		var upgradeResponse =
+//			'HTTP/' + proxyRes.httpVersion + ' ' +
+//			proxyRes.statusCode + ' ' +
+//			proxyRes.statusMessage + '\r\n' +
+//			common.headerLines(proxyRes) + '\r\n';
+//
+//		common.printIO('< ', upgradeResponse,
+//			proxySocket.remoteAddress, proxySocket.remotePort
+//		);
+//
+//		proxySocket.on('end', function () {
+//			short_server.close();
+//		});
+//		proxySocket.on('error', function (err) {
+//			console.log('ERR: ' + err.message);
+//		});
+//		socket.on('error', function () {
+//			proxySocket.end();
+//		});
+//
+//		if (proxyHead && proxyHead.length)
+//			proxySocket.unshift(proxyHead);
+//
+//		common.printIO('<<', upgradeResponse,
+//			socket.remoteAddress, socket.remotePort
+//		);
+//		console.log(upgradeResponse);
+//
+//		socket.write(upgradeResponse);
+//		proxySocket.pipe(socket).pipe(proxySocket);
+//		//long_socket.pipe(socket);
+//	});
 });
