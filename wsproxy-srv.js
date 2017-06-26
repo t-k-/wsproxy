58206178bef9849dad242b946a25842e6a7449d9
t-k- <clock126@126.com>
2017-06-26 16:19:38 +0800

    Multiplex working.

--- wsproxy-srv.js
+++ wsproxy-srv.js
@@ -4,89 +4,82 @@ var common = require('./wsproxy-common.js');
 
 var g_socket_id = 0;
 var pipe_listener = net.createServer().listen(8986);
-var pipes = {};
+var pipe_sockets = [];
 
-function print_pipes() {
-	Object.keys(pipes).forEach(function(id) {
-		console.log('pipe' + id + ' assoc:' + JSON.stringify(pipes[id].assoc));
-	});
+function pipe_alloc(assoc_id) {
+	for (var i = 0; i < pipe_sockets.length; i++) {
+		if (pipe_sockets[i].assoc_id == 0) {
+			pipe_sockets[i].assoc_id = assoc_id;
+			return pipe_sockets[i];
+		}
+	}
+
+	return null;
+}
+
+function pipe_free(assoc_id) {
+	for (var i = 0; i < pipe_sockets.length; i++) {
+		if (pipe_sockets[i].assoc_id == assoc_id) {
+			console.log('pipe socket' + assoc_id + ' ended.');
+			pipe_sockets[i].end();
+			return;
+		}
+	}
+
+	console.log('nothing ended.');
 }
 
 pipe_listener.on('connection', function(socket) {
-	console.log('one pipe accepted.');
+	console.log('new pipe accepted.');
 	common.setupSocket(socket);
-	socket.id = (++g_socket_id);
 
 	socket.on('error', function(err){
 		console.log('pipe ERR: ' + err.message);
 	});
 
-	pipes[socket.id] = {'assoc': {}, 'socket': socket};
-	print_pipes();
+	socket.assoc_id = 0;
+	pipe_sockets.push(socket);
 });
-
-//var short_server = http.createServer().listen(8999);
-
-//short_server.on('upgrade', function(req, socket, head) {
-//	socket.on('error', function(err){
-//		console.log('short ERR: ' + err.message);
-//	});
-//
-//	common.setupSocket(socket);
-//
-//	common.printIO('> ',
-//		req['method'] + ' ' + req['url'] + '\n' +
-//		common.headerLines(req),
-//		socket.remoteAddress, socket.remotePort
-//	);
-//
-//    if (head && head.length) socket.unshift(head);
-//	
-//	var tmpMsg =
-//		req.method + ' ' + req.url + '\r\n' +
-//		common.headerLines(req) + '\r\n';
-//
-//
-//	if (long_socket) {
-//		common.printIO('>>', tmpMsg,
-//			long_socket.remoteAddress, long_socket.remotePort
-//		);
-//
-//		long_socket.write(tmpMsg);
-//		///////////// long_socket.end();
-//
-//		socket.pipe(long_socket)
-//		long_socket.pipe(socket);
-//	}
-//});
 
 var pub_listener = http.createServer().listen(8999);
 pub_listener.on('upgrade', function(req, socket, head) {
-	peer_key = {
-		'IP': socket.remoteAddress,
-		'port': socket.remotePort,
-		'UsrAgent': req.headers['user-agent'],
-	};
-	common.setupSocket(socket);
-	console.log(peer_key);
-//	common.printIO('> ',
-//		req['method'] + ' ' + req['url'] + '\n' +
-//		common.headerLines(req),
-//		socket.remoteAddress, socket.remotePort
-//	);
-});
+//	peer_info = {
+//		'IP': socket.remoteAddress,
+//		'port': socket.remotePort,
+//		'UsrAgent': req.headers['user-agent'],
+//	};
+//	console.log(peer_info);
 
-//short_server.on('upgrade', function(req, socket, head) {
-//	socket.on('error', function(err){
-//		console.log('short ERR: ' + err.message);
-//	});
-//
-//	common.setupSocket(socket);
-//
-//	common.printIO('> ',
-//		req['method'] + ' ' + req['url'] + '\n' +
-//		common.headerLines(req),
-//		socket.remoteAddress, socket.remotePort
-//	);
-//
-//    if (head && head.length) socket.unshift(head);
+	common.setupSocket(socket);
+
+	/* allocate ID */
+	socket.assoc_id = (++g_socket_id);
+	console.log('new public socket' + socket.assoc_id);
+
+	/* allocate a free pipe socket */
+	var free_socket = pipe_alloc(socket.assoc_id);
+	if (free_socket == null) {
+		console.log('no free pipe');
+		return;
+	} else {
+		console.log('pipe allocated.');
+	}
+
+	/* forward this upgrade message first */
+	if (head && head.length) socket.unshift(head);
+	var upgradeMsg =
+		req.method + ' ' + req.url + '\r\n' +
+		common.headerLines(req) + '\r\n';
+	free_socket.write(upgradeMsg);
+
+	/* associate sockets for incomming traffic */
+	socket.pipe(free_socket);
+	free_socket.pipe(socket);
+
+	/* disassociate pipe on deletion */
+	socket.on('end', function() {
+		pipe_free(this.assoc_id);
+		this.destroy();
+		this.unref();
+	});
+});
