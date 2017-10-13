aeace6afb9491fd05d4e514473bf3caee333149e
t-k- <clock126@126.com>
2017-10-13 03:22:11 -0400

    Exit program on close/error.

--- wsproxy-cli.js
+++ wsproxy-cli.js
@@ -9,19 +9,21 @@ timelog('Remote host: ' + remote_host);
 var long_socket = new net.Socket();
 var short_socket = new net.Socket();
 
+var close_both_sockets = function () {
+	long_socket.destroy();
+	short_socket.destroy();
+};
+
 short_socket.connect(8989, '127.0.0.1', function() {
 	common.setupSocket(short_socket);
 	timelog('short socket connected');
 
 }).on('close', function() {
 	timelog('short socket closed !!!!!');
-	setTimeout(function () {
-		short_socket.connect(8989, '127.0.0.1', function() {
-			timelog('short socket connected again');
-		});
-	}, 2000);
+	close_both_sockets();
 }).on('error', function (err) {
 	timelog('short socket ERR: ' + err.message);
+	close_both_sockets();
 }).on('data', function(chunk) {
 	timelog('short socket on data...');
 	long_socket.write(chunk);
@@ -33,13 +35,10 @@ long_socket.connect(8986, remote_host, function() {
 
 }).on('close', function() {
 	timelog('long socket closed !!!!!');
-	setTimeout(function () {
-		long_socket.connect(8986, remote_host, function() {
-			timelog('long socket connected again');
-		});
-	}, 2000);
+	close_both_sockets();
 }).on('error', function (err) {
 	timelog('long socket ERR: ' + err.message);
+	close_both_sockets();
 }).on('data', function(chunk) {
 	timelog('long socket on data...');
 	short_socket.write(chunk);
